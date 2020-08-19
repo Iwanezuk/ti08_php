7ff191896e4afb07c240a6409ad3c72340d87d1f
Iwanezuk <iwanezuk@hotmail.com>
2020-08-19 20:23:44 -0300

    Create menu_publico.php

---
+++ menu_publico.php
@@ -0,0 +1,41 @@
+<!-- Salvar como: menu_publico.php -->
+<!doctype html>
+<html lang="pt-br">
+<head>
+<title>Chuleta Quente</title>
+<meta charset="utf-8">
+<!-- Link arquivos Bootstrap css -->
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link href="css/bootstrap.min.css" rel="stylesheet">
+<link href="css/meu_estilo.css" rel="stylesheet">
+</head>
+<body>
+<!-- Abre a barra de navegação -->
+<nav class="navbar navbar-inverse">
+<div class="container-fluid">
+   <!-- Agrupamento MOBILE -->
+   <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#menuPublico" aria-expanded="false">
+         <span class="sr-only">Navegação mobile</span>
+         <span class="icon-bar"></span>
+         <span class="icon-bar"></span>
+         <span class="icon-bar"></span>
+      </button>
+       
+   </div>
+    
+</div>
+</nav>
+
+
+
+
+
+
+
+<!-- Link arquivos Bootstrap js -->        
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
