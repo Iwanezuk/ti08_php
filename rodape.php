206fca80624a35d9b53f825d0fdbefb07c88628e
Iwanezuk <iwanezuk@hotmail.com>
2020-08-25 19:57:42 -0300

    Create rodape.php

---
+++ rodape.php
@@ -0,0 +1,95 @@
+<!-- Salvar como: rodape.php -->
+<!doctype html>
+<html lang="pt-br">
+<head>
+<title>Chuleta Quente</title>
+<meta charset="utf-8">
+<!-- Link arquivos Bootstrap css -->
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link href="css/bootstrap.min.css" rel="stylesheet">
+<link href="css/meu_estilo.css" rel="stylesheet">
+</head>
+<body class="fundofixo">
+<!-- Abre o painel de Rodapé -->
+<div class="row panel-footer" style="background-color:rgba(255,255,255,0.8)">
+<!-- Abre ÁREA DE LOCALIZAÇÃO -->
+<div class="col-sm-6 col-md-4">
+   <div class="panel-footer" style="background:none">
+      <img src="imagens/logochurrascopequeno.png" alt="">
+      <br>
+      <i>O melhor churrasco da região!</i>
+      <address>
+          <i>Rua Dom Joaquim, 495 - Centro - Itapetininga - SP - CEP 18200-000</i>
+          <br>
+          <span class="glyphicon glyphicon-phone-alt"></span>&nbsp;Fone (15) 3511 1200
+          <br>
+          <span class="glyphicon glyphicon-envelope"></span>&nbsp;Email:
+          <a href="mailto:contato@chuletaquente.com.br?subject=Contato&cc=seuemail@hotmail.com">
+              contato@chuletaquente.com.br
+          </a>
+      </address>
+      <div class="embed-responsive embed-responsive-16by9">
+         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3656.3527067162086!2d-48.054867884475236!3d-23.591680368598958!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94c5cc93b46246ed%3A0x6ec0870ce87bb6fd!2sSenac%20Itapetininga!5e0!3m2!1spt-BR!2sbr!4v1568163169642!5m2!1spt-BR!2sbr" frameborder="0" style="border:0;" allowfullscreen=""></iframe> 
+      </div>
+   </div>
+</div>
+<!-- Fecha ÁREA DE LOCALIZAÇÃO -->
+
+<!-- Abre ÁREA DE NAVEGAÇÃO -->
+<div class="col-sm-6 col-md-4">
+   <div class="panel-footer" style="background:none">
+     <h4>Links</h4>
+     <!-- ul>li>a>span -->
+     <ul class="nav nav-pills nav-stacked">
+         <li>
+             <a href="index.php#home" class="text-danger">
+                 <span class="glyphicon glyphicon-home" aria-hidden="true">&nbsp;HOME</span>
+             </a>
+         </li>
+         <li>
+             <a href="index.php#destaques" class="text-danger">
+                 <span class="glyphicon glyphicon-ok-sign" aria-hidden="true">&nbsp;DESTAQUES</span>
+             </a>
+         </li>
+         <li>
+             <a href="index.php#produtos" class="text-danger">
+                 <span class="glyphicon glyphicon-cutlery" aria-hidden="true">&nbsp;PRODUTOS</span>
+             </a>
+         </li>
+         <li>
+             <a href="index.php#contato" class="text-danger">
+                 <span class="glyphicon glyphicon-envelope" aria-hidden="true">&nbsp;CONTATO</span>
+             </a>
+         </li>
+         <li>
+             <a href="admin/index.php" class="text-danger">
+                 <span class="glyphicon glyphicon-user" aria-hidden="true">&nbsp;ADMINISTRAÇÃO</span>
+             </a>
+         </li>
+         
+         
+         
+     </ul>
+      
+   </div>
+</div>
+<!-- Fecha ÁREA DE NAVEGAÇÃO -->
+
+<!-- Abre ÁREA FORM CONTATO -->
+<div class="col-sm-6 col-md-4">
+   <div class="panel-footer" style="background:none">
+     <h4>Contato</h4>
+      
+   </div>
+</div>
+<!-- Fecha ÁREA FORM CONTATO -->                        
+    
+</div>
+<!-- Fecha o painel de Rodapé -->
+
+<!-- Link arquivos Bootstrap js -->        
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
