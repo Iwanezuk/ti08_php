456a142c7ad69e97605d35e46eaa7f3c79418a5f
Iwanezuk <iwanezuk@hotmail.com>
2020-08-26 22:44:21 -0300

    Update produtos_geral.php

--- produtos_geral.php
+++ produtos_geral.php
@@ -22,10 +22,12 @@ $totalRows  =   ($lista)->num_rows;
 <title>Área Administrativa</title>
 <meta charset="utf-8">
 <!-- Link arquivos Bootstrap css -->
+<!-- CÓDIGO DESABILITADO PARA NÃO HAVER CONFLITOS
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link href="css/bootstrap.min.css" rel="stylesheet">
 <link href="css/meu_estilo.css" rel="stylesheet">
+-->
 </head>
 <body>
 <h2 class="breadcrumb alert-danger">Produtos</h2>
@@ -65,9 +67,11 @@ $totalRows  =   ($lista)->num_rows;
 <!-- Fecha estrutura de repetição -->  
 </div><!-- fecha row -->
 
-<!-- Link arquivos Bootstrap js -->        
+<!-- Link arquivos Bootstrap js -->
+<!-- CÓDIGO DESABILITADO PARA NÃO HAVER CONFLITOS        
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 <script src="js/bootstrap.min.js"></script>
+-->
 </body>
 </html>
 <?php mysqli_free_result($lista); ?>
